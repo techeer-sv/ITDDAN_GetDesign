4bcbe4b6ccf7131dcce733ab2878ab43f299f613
Jungkihong07 <hnn07185@gmail.com>
2025-08-24 03:34:09 +0900

    test 진행.

---
+++ kihong/week7/test_python.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+테스트용 Python 파일
+"""
+
+
+def main():
+    print("Hello, World!")
+    print("이 파일은 테스트용입니다.")
+    return True
+
+
+if __name__ == "__main__":
+    main()
