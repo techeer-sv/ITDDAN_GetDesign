c8710a21a96dcdbf595b468052bf89d19b4552df
Jungkihong07 <hnn07185@gmail.com>
2025-08-24 04:53:25 +0900

    test 용 파일 커밋.

--- kihong/week7/test_python.py
+++ kihong/week7/test_python.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-테스트용 Python 파일
+테스트용 Python 파일 - 워크플로우 테스트
 """
 
 
 def main():
-    print("Hello, World!ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ")
+    print("Hello, World! 워크플로우 테스트")
     print("이 파일은 테스트용입니다.")
+    print("GITHUB_JOB_STATUS 문제 해결 테스트")
     return True
 
 
